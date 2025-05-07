61df8f3f5acc39a653053866a2db434933cfaa4f
Fahad Alwardi <145479819+Fhd-1@users.noreply.github.com>
2025-05-07 15:29:44 +0300

    Update firebaseConfig.js

--- SeniorProject-src/src/services/firebaseConfig.js
+++ SeniorProject-src/src/services/firebaseConfig.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 // import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBu23uX-LYNJ0oK61o-2xI_2v_oylM4yvI",
+  apiKey: "",
   authDomain: "collab-platform-30b9b.firebaseapp.com",
   projectId: "collab-platform-30b9b",
   storageBucket: "collab-platform-30b9b.appspot.com", // fixed typo
-  messagingSenderId: "342508034805",
-  appId: "1:342508034805:web:047d1d139ce835ae6743bc",
+  messagingSenderId: "",
+  appId: "",
   measurementId: "G-7WR5KTRPP8", // optional, not needed unless using analytics
 };
 
